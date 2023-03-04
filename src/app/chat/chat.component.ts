68fd4cdd0f68b0c6825d5cb26ed285c9e14688ac
xketris <xketris@gmail.com>
2023-03-05 00:59:47 +0100

    chat layout

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  host: {
+    class:'chat-component'
+  },
+  styleUrls: ['./chat.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ChatComponent implements OnInit {
 
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
+import { ChatRoomListComponent } from './chat-room-list/chat-room-list.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 
 @NgModule({
   declarations: [
-    ChatComponent
+    ChatComponent,
+    ChatRoomListComponent,
+    ChatRoomComponent
   ],
   imports: [
     CommonModule,

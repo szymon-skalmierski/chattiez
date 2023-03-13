a76067d1ffd362359c10fb08d9e05538f96a6f96
xketris <xketris@gmail.com>
2023-03-13 23:52:21 +0100

    chat guard fix

--- src/app/chat/chat.guard.ts
+++ src/app/chat/chat.guard.ts
@@ -1,17 +1,21 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, Subscription } from "rxjs";
 import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class ChatGuard implements CanActivate {
+    isLoggedIn!: boolean
     constructor(private authService:AuthService, private router: Router){
+        this.authService.user.subscribe((user:any)=>{
+            this.isLoggedIn = !!user
+        })
     }
 
-    userStatus = this.authService.user
-
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if(this.userStatus) return true;
+        if (this.isLoggedIn){
+            return true
+        }
         this.router.navigate(['/auth'])
         return false;
     }

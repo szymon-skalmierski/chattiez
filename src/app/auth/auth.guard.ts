ece826955c5c895f13fa298989a8bfec79c2daf5
xketris <xketris@gmail.com>
2023-07-10 22:22:32 +0200

    simplifying auth guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,10 +9,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (!(localStorage.getItem('userData'))){
-        return true;
-      }
-      return false;
+      return !localStorage.getItem('userData');
   }
   
 }

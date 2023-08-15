f163fa813823e4a545e183265371742fcb66e5d9
xketris <xketris@gmail.com>
2023-08-15 18:53:20 +0200

    chat guard redirection of not logged in user

--- src/app/chat/chat.guard.ts
+++ src/app/chat/chat.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 
 import { Observable, filter, map } from "rxjs";
 
@@ -7,9 +7,13 @@ import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class ChatGuard  {
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private router: Router){}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+      if(this.authService.user.value===null) {
+        this.router.navigate(['/auth', 'login']);
+        return false;
+      }
       return (this.authService.user.pipe(filter(value => value!==undefined), map(value => !!value)) as Observable<boolean>)
     }
 }

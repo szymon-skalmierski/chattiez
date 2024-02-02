85422e4eee5de27d237724a608f3301620bedc0d
szymon-skalmierski <xketris@gmail.com>
2024-02-03 00:27:01 +0100

    set-env file

---
+++ src/environments/set-env.ts
@@ -0,0 +1,29 @@
+const setEnv = () => {
+    const fs = require('fs');
+    const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+    const targetPath = './src/environments/environment.prod.ts';
+  // Load node modules
+    const appVersion = require('../../package.json').version;
+    require('dotenv').config({
+      path: 'src/environments/.env'
+    });
+  // `environment.ts` file structure
+    const envConfigFile = `export const environment = {
+    APP_ID: '${process.env.APP_ID}',
+    FIREBASE_KEY: '${process.env.FIREBASE_KEY}',
+    production: true,
+  };
+  `;
+  console.log('The file `environment.ts` will be written with the following content:');
+    writeFile(targetPath, envConfigFile, (err: any) => {
+      if (err) {
+        console.error(err);
+        throw err;
+      } else {
+        console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+      }
+    });
+  };
+  
+  setEnv();
